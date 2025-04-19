0b9e7765df8801ea0b758f1b904f100cacc3b9a2
João Bertholino <comercial.bertholino@gmail.com>
2025-04-19 08:48:22 -0300

    Configure to psn00bsdk

--- custom.el
+++ custom.el
@@ -4,27 +4,23 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ignored-local-variable-values
-   '((eval let
-      ((root
-        (projectile-project-root)))
+   '((eval let ((root (projectile-project-root)))
       (let
           ((includes
-            (list "/opt/psn00bsdk/include/libpsn00b"
-                  (concat root "include")))
+            (list "/opt/psn00bsdk/include/libpsn00b" (concat root "include")))
            (neotreebuf
-            (seq-filter
-             (lambda
-               (buf)
-               (equal
-                (buffer-name buf)
-                " *NeoTree*"))
-             (buffer-list))))
+            (seq-filter (lambda (buf) (equal (buffer-name buf) " *NeoTree*"))
+                        (buffer-list))))
         (setq-local flycheck-clang-include-path includes)
         (setq-local flycheck-gcc-include-path includes)
         (dap-register-debug-template "PSX Debug"
-                                     (list :name "PSX -- Engine debug" :type "gdbserver" :request "attach" :gdbpath "/usr/bin/gdb-multiarch" :target ":3333" :cwd root))))))
+                                     (list :name "PSX -- Engine debug" :type
+                                           "gdbserver" :request "attach" :gdbpath
+                                           "/usr/bin/gdb-multiarch" :target
+                                           ":3333" :cwd root))))))
  '(package-selected-packages
-   '(auctex evil-collection company-spell company lsp-java lsp-ui auto-complete daemons vterm)))
+   '(auctex auto-complete cargo-mode company company-spell daemons evil-collection
+     lsp-java lsp-ui rust-mode vterm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
